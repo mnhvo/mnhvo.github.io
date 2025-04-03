d3caf7aa711b93b00a825e28a47da1a3ef67d970
Minh Vo <minhav131@gmail.com>
2025-04-03 12:05:08 +1100

    week 5 activity

---
+++ week5/script.js
@@ -0,0 +1,46 @@
+console.log("Hello");
+let myName = "Rohit";
+console.log("Hello", myName);
+myName = "Sarah";
+console.log("Hello", myName);
+let id = "s1234";
+console.log("My id is", id);
+
+//let myCity = prompt("Wh");
+//not right practice
+
+//numerical variables 0-9
+let a = 10;
+let b = 20;
+let c = a + b;
+console.log(c);
+//+ addition - subtraction / division * mulitplication
+
+//string variable text '' "" ``
+let myName1 = "Minh Vo";
+console.log("hello", myName);
+let myCity = "melbourne";
+
+//boolean variable
+let isItSunday = false;
+let isIt0ART1013 = true;
+
+//object variables
+const myStudentRecord = {
+  name: "Minh",
+  id: 1234,
+  homeTown: "Melbourne",
+  isLocal: false,
+};
+console.log(myStudentRecord);
+console.log(myStudentRecord.homeTown);
+
+//arrays
+
+//if condition
+let grade = 5;
+if (grade > 70) {
+  console.log("You got a HD");
+} else {
+  console.log("You passed");
+}
